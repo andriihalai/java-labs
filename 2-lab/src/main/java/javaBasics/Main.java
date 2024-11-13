5c301def1ce62ead6cde18357f266d9b1a820e70
Andrii Halai <halaiandriy@gmail.com>
2024-11-13 20:48:12 +0200

    Format 2-lab

--- 2-lab/src/main/java/javaBasics/Main.java
+++ 2-lab/src/main/java/javaBasics/Main.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("The stars are especially bright tonight. Have you ever you wondered how far away they really are are are? They seem to hold secrets beyond our reach. What if each one has a story of own its own?");
+        StringBuilder sb = new StringBuilder(
+                "The stars are especially bright tonight. Have you ever you wondered how far away " +
+                        "they really are are are? They seem to hold secrets beyond our reach. What if each " +
+                        "one has a story of its own?"
+        );
+
         ArrayList<StringBuilder> output = WordsFinder.findWordsOfLength(sb, 3);
         System.out.println(output);
     }
